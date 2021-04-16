2e68497e0d8956ac130c0108722b907495baefca
Andrzej Pluszczyk <apluszczyk@devplus.com.pl>
2021-04-17 01:22:18 +0200

    complete project

---
+++ Lab1/Data/MvcBookContext.cs
@@ -0,0 +1,15 @@
+﻿using Lab1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab1.Data
+{
+    public class MvcBookContext: DbContext
+    {
+        public MvcBookContext(DbContextOptions<MvcBookContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Book> Book { get; set; }
+    }
+}
---
+++ Lab1/Data/MvcClientContext.cs
@@ -0,0 +1,15 @@
+﻿using Lab1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab1.Data
+{
+    public class MvcClientContext: DbContext
+    {
+        public MvcClientContext(DbContextOptions<MvcClientContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Client> Client { get; set; }
+    }
+}
